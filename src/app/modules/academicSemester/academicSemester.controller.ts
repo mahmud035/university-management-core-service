4f17676733c47e5efd6bd2f561cde0a5e987bab3
mahmud035 <mahamudulhasan4148@gmail.com>
2023-08-21 21:12:03 +0600

    35-6: Improved Functionality with catchAsync and Validation for AcademicSemesters

--- src/app/modules/academicSemester/academicSemester.controller.ts
+++ src/app/modules/academicSemester/academicSemester.controller.ts
@@ -1,15 +1,12 @@
 import { AcademicSemester } from '@prisma/client';
-import { NextFunction, Request, Response } from 'express';
+import { Request, Response } from 'express';
 import httpStatus from 'http-status';
+import catchAsync from '../../../shared/catchAsync';
 import sendResponse from '../../../shared/sendResponse';
 import { AcademicSemesterService } from './academicSemester.services';
 
-const createAcademicSemester = async (
-  req: Request,
-  res: Response,
-  next: NextFunction
-) => {
-  try {
+const createAcademicSemester = catchAsync(
+  async (req: Request, res: Response) => {
     const result = await AcademicSemesterService.createAcademicSemester(
       req.body
     );
@@ -20,10 +17,8 @@ const createAcademicSemester = async (
       message: 'Academic Semester created successfully',
       data: result,
     });
-  } catch (error) {
-    next(error);
   }
-};
+);
 
 export const AcademicSemesterController = {
   createAcademicSemester,
--- src/app/modules/academicSemester/academicSemester.route.ts
+++ src/app/modules/academicSemester/academicSemester.route.ts
@@ -1,10 +1,13 @@
 import express from 'express';
+import validateRequest from '../../middlewares/validateRequest';
 import { AcademicSemesterController } from './academicSemester.controller';
+import { AcademicSemesterZodValidation } from './academicSemester.validation';
 
 const router = express.Router();
 
 router.post(
   '/create-semester',
+  validateRequest(AcademicSemesterZodValidation.create),
   AcademicSemesterController.createAcademicSemester
 );
 

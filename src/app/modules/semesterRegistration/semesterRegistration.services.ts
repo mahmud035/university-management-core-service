7f22a3f828c6dd02ab3a83253895073033cd087d
Md. Mahamudul Hasan <72532498+mahmud035@users.noreply.github.com>
2023-09-07 13:33:59 +0600

    37-10_ Strengthening Validation in Semester Registration Service

--- src/app/modules/semesterRegistration/semesterRegistration.services.ts
+++ src/app/modules/semesterRegistration/semesterRegistration.services.ts
@@ -1,9 +1,35 @@
-import { SemesterRegistration } from '@prisma/client';
+import {
+  SemesterRegistration,
+  SemesterRegistrationStatus,
+} from '@prisma/client';
+import httpStatus from 'http-status';
+import ApiError from '../../../errors/ApiError';
 import prisma from '../../../shared/prisma';
 
 const createSemesterRegistration = async (
   data: SemesterRegistration
 ): Promise<SemesterRegistration> => {
+  const isAnySemesterRegUpcomingOrOngoing =
+    await prisma.semesterRegistration.findFirst({
+      where: {
+        OR: [
+          {
+            status: SemesterRegistrationStatus.UPCOMING,
+          },
+          {
+            status: SemesterRegistrationStatus.ONGOING,
+          },
+        ],
+      },
+    });
+
+  if (isAnySemesterRegUpcomingOrOngoing) {
+    throw new ApiError(
+      httpStatus.BAD_REQUEST,
+      `There is already an ${isAnySemesterRegUpcomingOrOngoing.status} registration.`
+    );
+  }
+
   const result = await prisma.semesterRegistration.create({
     data,
   });

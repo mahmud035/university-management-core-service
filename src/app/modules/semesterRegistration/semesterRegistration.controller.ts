9e85d1d16f32fb32d92c3315a5956738e5ae24c4
Md. Mahamudul Hasan <72532498+mahmud035@users.noreply.github.com>
2023-09-07 13:24:13 +0600

    37-9_ Semester Registration Creation

---
+++ src/app/modules/semesterRegistration/semesterRegistration.controller.ts
@@ -0,0 +1,24 @@
+import { Request, Response } from 'express';
+import httpStatus from 'http-status';
+import catchAsync from '../../../shared/catchAsync';
+import sendResponse from '../../../shared/sendResponse';
+import { SemesterRegistrationService } from './semesterRegistration.services';
+
+const createSemesterRegistration = catchAsync(
+  async (req: Request, res: Response) => {
+    const result = await SemesterRegistrationService.createSemesterRegistration(
+      req.body
+    );
+
+    sendResponse(res, {
+      statusCode: httpStatus.OK,
+      success: true,
+      message: 'Semester Registration created successfully',
+      data: result,
+    });
+  }
+);
+
+export const SemesterRegistrationController = {
+  createSemesterRegistration,
+};

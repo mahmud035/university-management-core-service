e8b2feeba47735b49fa1b74cced7df20378a8aec
Md. Mahamudul Hasan <72532498+mahmud035@users.noreply.github.com>
2023-09-29 19:25:03 +0600

    added OfferedCourse model

---
+++ src/app/modules/offeredCourse/offeredCourse.controller.ts
@@ -0,0 +1,6 @@
+// import { Request, Response } from "express";
+// import catchAsync from "../../../shared/catchAsync";
+
+// const createOfferedCourse = catchAsync(async (req: Request, res: Response) => {
+//   const result = await
+// })
---
+++ src/app/modules/offeredCourse/offeredCourse.services.ts
@@ -0,0 +1,12 @@
+// import { OfferedCourse } from '@prisma/client';
+// import { ICreateOfferedCourse } from './offeredCourse.interface';
+
+// const createOfferedCourse = async (
+//   data: ICreateOfferedCourse
+// ): Promise<OfferedCourse[]> => {
+//   const { academicDepartmentId, semesterRegistrationId, courseIds } = data;
+
+//   const result: OfferedCourse[] = [];
+
+//   await
+// };
